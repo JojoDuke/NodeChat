92703e3652657e38736740072b858a208622ff89
JojoDuke <jojoamankwa@gmail.com>
2022-02-24 02:26:06 +0000

    ciad

--- client/src/App.js
+++ client/src/App.js
@@ -35,23 +35,28 @@ function App() {
           </Typography>
           <div>
             <button className='nav-btns white'>How to use</button>
-            <button className='nav-btns black'>Follow me on Twitter...</button>
+            <button 
+              className='nav-btns black'
+              onClick={(e) => {
+                e.preventDefault();
+                window.location.href='https://twitter.com/itsjojoduke1';
+              }}>Follow me on Twitter...</button>
           </div>
         </Toolbar>
       </AppBar>
 
       {/* HERO SECTION */}
-      <Container maxWidth="xl" sx={{ textAlign: "center", }}>
-        <Typography variant='h4'>NodeChat is a simple live chat app made with React and Node</Typography>
-        <Typography variant='h6'>Just open the app in another window and make sure to join the same room</Typography>
+      <Container maxWidth="xl" sx={{ textAlign: "center", marginTop: "60px", }}>
+        <Typography variant='h2' fontWeight="600">NodeChat is a live chat app made<br/> with React and Node</Typography>
+        <Typography variant='h6' sx={{ color: "gray", }}>Realtime chat using Sockets.io, just open the app<br/> in another window and make sure to join the same room, its that easy</Typography>
       </Container>
 
       {!showChat ? (
         <div className='joinChat-div'>
           <h3>Join A Chat</h3>
-        <input type="text" placeholder="Name..." onChange={ (e) => {setUsername(e.target.value)} }/>
-        <input type="text" placeholder="Room ID..." onChange={ (e) => {setRoom(e.target.value)} } />
-        <button onClick={joinRoom}>Join A Room</button>
+        <input type="text" placeholder="Name" onChange={ (e) => {setUsername(e.target.value)} }/>
+        <input type="text" placeholder="Room ID" onChange={ (e) => {setRoom(e.target.value)} } />
+        <button className='nav-btns black join-btn' onClick={joinRoom}>Join A Room</button>
       </div>) : (<Chats socket={socket} username={username} room={room}/>)}
     </div>
   );
